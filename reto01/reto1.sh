0be20b33270f05574b5efb6efba53e5d254d9277
E10697 <cesar.justo@telefonica.com>
2022-08-17 21:26:50 -0400

    Se sube reto 2

--- reto01/reto1.sh
+++ reto01/reto1.sh
@@ -6,18 +6,4 @@ docker run -d --name servidor_web -p 8181:80 nginx
 # Listando todos los contenedores arriba
 docker ps
 
-# Listando todas las imagenes
-docker images
 
-# Detienendo el contenedor
-docker stop servidor_web
-
-# Listando todos los contenedores arriba
-docker ps
-
-# Eliminando el contenedor anterior
-docker rm servidor_web
-
-# Listando todos los contenedores arriba/detenidos/fuera
-
-docker ps -a
---
+++ reto02/solucion.md
@@ -0,0 +1,20 @@
+# **Solucion Reto 2 - Challenge 2 Bootcamp DEVOPS**
+
+---
+## **Conexion BD y BD creada**
+
+[Conexion BD](img/imagen1.png)
+
+[BD creada](img/imagen2.png)
+
+---
+## **No se puede eliminar la imagen**
+
+[Imagen](img/imagen3.png)
+
+
+
+
+
+
+
